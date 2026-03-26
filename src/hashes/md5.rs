c534004dc583a6a69cbc0a6697545ccab84cdaaf
Erik <erikgavs@gmail.com>
2026-03-26 12:31:05 +0100

    md5 salt function applied

--- src/hashes/md5.rs
+++ src/hashes/md5.rs
@@ -1,3 +1,7 @@
 pub fn crack(word: &str) -> String {
     format!("{:x}", md5::compute(word))
 }
+
+pub fn crack_with_salt(word: &str, salt: &str) -> String {
+    let salted = format!("{}{}", salt, word);
+}
