d1ce55939d8b75a4518aac760837e9cb5a20f53d
Erik <erikgavs@gmail.com>
2026-03-26 13:43:29 +0100

    cargo test updated, now cargo test tries to use all the tools

--- src/hashes/md5.rs
+++ src/hashes/md5.rs
@@ -6,3 +6,20 @@ pub fn crack_with_salt(word: &str, salt: &str) -> String {
     let salted = format!("{}{}", salt, word);
     format!("{:x}", md5::compute(salted))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_crack() {
+        assert_eq!(crack("password"), "5f4dcc3b5aa765d61d8327deb882cf99");
+        assert_eq!(crack("admin"), "21232f297a57a5a743894a0e4a801fc3");
+    }
+
+    #[test]
+    fn test_crack_with_salt() {
+        assert_eq!(crack_with_salt("password", "x7k2"), "86f75bc83edcd705c834c436f6b64fdc");
+        assert_eq!(crack_with_salt("admin", "r9f1"), "646f865e5ec771044931d7b00f4f2c25");
+    }
+}
--- src/hashes/sha512.rs
+++ src/hashes/sha512.rs
@@ -12,3 +12,20 @@ pub fn crack_with_salt(word: &str, salt: &str) -> String {
     hash_engine.update(salted);
     format!("{:x}", hash_engine.finalize())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_crack() {
+        assert_eq!(crack("password"), "b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86");
+        assert_eq!(crack("admin"), "c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec");
+    }
+
+    #[test]
+    fn test_crack_with_salt() {
+        assert_eq!(crack_with_salt("password", "x7k2"), "16840f66c91412ad31d522935d66b54d423c4f240aca10480cef20436a11d119e29ad82b761e8eafb8abf2bfabbe0aebb4732c76267225f6ad178d8896a9018f");
+        assert_eq!(crack_with_salt("admin", "r9f1"), "7e42c96c35b99d814b548fcf3cbdf65af998e2be9422c51c50d0dab2cbf2c370f6cf474ab9d8c1d4abb3189fea5c8ad9d3d67b2ca952e2b61b3024b6b0cdfebe");
+    }
+}
