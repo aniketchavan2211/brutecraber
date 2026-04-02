897d02c264260956e968ae3d17edd31c3dba97e8
Erik <erikgavs@gmail.com>
2026-04-02 11:08:24 +0200

    fix: more intuitive auto-detect

--- src/detector.rs
+++ src/detector.rs
@@ -2,7 +2,7 @@ pub fn detect(hash: &str) -> &str {
     match hash.len() {
         32 => "md5",
         40 => "sha1",
-        64 => "sha256-auto",
+        64 => "sha256 / sha3-256",
         128 => "sha512",
         _ => "hash not recognized, try using -t parameter",
     }
